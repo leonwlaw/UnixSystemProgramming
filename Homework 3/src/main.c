1f9e9776ec72de05df95384694c4e2393c89df87
Leonard Law <leon.wlaw@gmail.com>
2013-09-23 00:07:13 -0400

    Space -> Tabs

--- Homework 3/src/main.c
+++ Homework 3/src/main.c
@@ -21,50 +21,50 @@ char *EXIT_COMMAND = "exit\n";
 // If the buffer size is insufficient to store all the tokens,
 // this function will return 1.
 int tokenize(char *string, char **tokens) {
-  int tokenNumber = 0;
-  char *token;
-  for (token = strtok(string, TOKEN_DELIMITERS);
-    // Last spot in the token buffer is reserved for NULL
-    token != NULL && tokenNumber < TOKEN_BUFFERSIZE - 1;
-    token = strtok(NULL, TOKEN_DELIMITERS), ++tokenNumber)
-  {
-    tokens[tokenNumber] = token;
-  }
-  tokens[tokenNumber + 1] = NULL;
+	int tokenNumber = 0;
+	char *token;
+	for (token = strtok(string, TOKEN_DELIMITERS);
+		// Last spot in the token buffer is reserved for NULL
+		token != NULL && tokenNumber < TOKEN_BUFFERSIZE - 1;
+		token = strtok(NULL, TOKEN_DELIMITERS), ++tokenNumber)
+	{
+		tokens[tokenNumber] = token;
+	}
+	tokens[tokenNumber + 1] = NULL;
 
-  // Did we tokenize everything?
-  if (token != NULL)
-    return 1;
-  return 0;
+	// Did we tokenize everything?
+	if (token != NULL)
+		return 1;
+	return 0;
 }
 
 
 void printStrings(char** printStrings) {
-  for (; *printStrings != NULL; ++printStrings) {
-    puts(*printStrings);
-  }
+	for (; *printStrings != NULL; ++printStrings) {
+		puts(*printStrings);
+	}
 }
 
 
 int main(int argc, char const *argv[])
 {
-  char *input = malloc(sizeof(char) * INPUT_BUFFERSIZE);
-  char *prompt = malloc(sizeof(char) * PROMPT_BUFFERSIZE);
-  char **tokens = malloc(sizeof(char *) * TOKEN_BUFFERSIZE);
+	char *input = malloc(sizeof(char) * INPUT_BUFFERSIZE);
+	char *prompt = malloc(sizeof(char) * PROMPT_BUFFERSIZE);
+	char **tokens = malloc(sizeof(char *) * TOKEN_BUFFERSIZE);
 
-  strncpy(prompt, DEFAULT_PROMPT, PROMPT_BUFFERSIZE);
+	strncpy(prompt, DEFAULT_PROMPT, PROMPT_BUFFERSIZE);
 
-  while (strcmp(input, EXIT_COMMAND) != 0)
-  {
-    fputs(prompt, stdout);
-    fgets(input, INPUT_BUFFERSIZE, stdin);
+	while (strcmp(input, EXIT_COMMAND) != 0)
+	{
+		fputs(prompt, stdout);
+		fgets(input, INPUT_BUFFERSIZE, stdin);
 
-    if (tokenize(input, tokens) != 0) {
-      fputs("Warning: Not all tokens were tokenized successfully.\n", stderr);
-    }
-  }
+		if (tokenize(input, tokens) != 0) {
+			fputs("Warning: Not all tokens were tokenized successfully.\n", stderr);
+		}
+	}
 
-  free(input);
-  free(prompt);
-  return 0;
+	free(input);
+	free(prompt);
+	return 0;
 }
