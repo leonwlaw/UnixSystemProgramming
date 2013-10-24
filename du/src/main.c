76eb232e064e668f3c86d0850cc169220a88841f
Leonard Law <leon.wlaw@gmail.com>
2013-10-24 01:52:28 -0400

    Added support for directory iteration

--- du/src/main.c
+++ du/src/main.c
@@ -11,22 +11,37 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <dirent.h>
 #include <unistd.h>
 
 const int STAT_FAILED = 1;
+const int OPENDIR_FAILED = 2;
 
 int main(int argc, char *argv) {
-  // Retrieve directory information for the current directory
-  struct stat dirstat;
-  if (lstat(".", &dirstat) != 0) {
+  // Now check each file entry...
+  DIR *directory;
+  if ((directory = opendir(".")) == NULL) {
     perror("du");
-    exit(STAT_FAILED);
+    exit(OPENDIR_FAILED);
   }
 
-  // Each block is 512B large. Therefore, if we divide the # of blocks
-  // by 2, we get the size in KB.
-  unsigned size = (int)dirstat.st_blocks / 2;
-  fprintf(stdout, "%-8d.\n", size);
+  for (struct dirent *directoryEntry = readdir(directory);
+       directoryEntry != NULL;
+       directoryEntry = readdir(directory)) {
+
+    // Retrieve directory information for the current directory
+    struct stat dirstat;
+    if (lstat(directoryEntry->d_name, &dirstat) != 0) {
+      perror("du");
+      exit(STAT_FAILED);
+    }
+
+    // Each block is 512B large. Therefore, if we divide the # of blocks
+    // by 2, we get the size in KB.
+    unsigned size = (int)dirstat.st_blocks / 2;
+    fprintf(stdout, "%-8d%s\n", size, directoryEntry->d_name);
+  }
 
   return 0;
 }
+
