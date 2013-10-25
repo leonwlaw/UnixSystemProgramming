dfcc49d121081304d8a20e6f283895cee42b8b8b
Leonard Law <leon.wlaw@gmail.com>
2013-10-24 23:12:12 -0400

    Count multiple hard links only once

--- du/src/main.c
+++ du/src/main.c
@@ -20,30 +20,45 @@ const int STAT_FAILED = 1;
 const int OPENDIR_FAILED = 2;
 const int CLOSEDIR_FAILED = 3;
 const int MEMORY_ALLOC_FAILED = 4;
+const int SEEN_INODE_BUFFER_OVERFLOW = 5;
 
 // The size of the temporary array to hold the child path at each iteration.
 // 1023 characters should be plenty, I hope.
 const size_t CHILD_PATH_BUFFER_SIZE = 4096;
 
+const int SEEN_INODE_BUFFER_SIZE = 1024;
+
+
 /* Function Stubs
  */
+
+/* Checks to see if the specified inode is in the array.
+ */
+bool isInInodesArray(ino_t inode, ino_t *seenInodes, size_t length);
 
 /* Calculates the disk usage for the specified directory.
  * Prints out the disk usage for child directories.
  */
-int diskUsage(char *directoryPath);
+int diskUsage(char *directoryPath, ino_t **seenInodes, size_t *lastUsedInodeIndex, size_t seenInodesLength);
 
 
 int main(int argc, char **argv) {
   char *directoryPath = ".";
-
+  // Necessary to keep track of multiple hard links, so that we don't double count.
+  ino_t *seenInodes;
+  if ((seenInodes = calloc(SEEN_INODE_BUFFER_SIZE, sizeof(ino_t))) == NULL) {
+    perror("du");
+    exit(MEMORY_ALLOC_FAILED);
+  }
   if (argc > 1) {
     directoryPath = argv[1];
   }
-
-  int size = diskUsage(directoryPath);
+  size_t lastUsedInodeIndex = 0;
+  int size = diskUsage(directoryPath, &seenInodes, &lastUsedInodeIndex, SEEN_INODE_BUFFER_SIZE);
   fprintf(stdout, "%-8d%s\n", size, directoryPath);
 
+  // Free up anything we allocated...
+  free(seenInodes);
   return 0;
 }
 
@@ -51,7 +66,7 @@ int main(int argc, char **argv) {
 /* Function Definitions
  */
 
-int diskUsage(char *directoryPath) {
+int diskUsage(char *directoryPath, ino_t **seenInodes, size_t *lastUsedInodeIndex, size_t seenInodesLength) {
   // Now check each file entry...
   DIR *directory;
   if ((directory = opendir(directoryPath)) == NULL) {
@@ -105,7 +120,21 @@ int diskUsage(char *directoryPath) {
       // by 2, we get the size in KB.
       unsigned size = (int)dirstat.st_blocks / 2;
 
-      total += size;
+      if (dirstat.st_nlink == 1) {
+        total += size;
+      } else if (isInInodesArray(dirstat.st_ino, *seenInodes, seenInodesLength) == false) {
+        // This inode has not been seen before
+        total += size;
+
+        if (*lastUsedInodeIndex == seenInodesLength) {
+          perror("du");
+          exit(SEEN_INODE_BUFFER_OVERFLOW);
+        }
+
+        // Record this down so we know not to count this again.
+        *(*seenInodes + *lastUsedInodeIndex) = dirstat.st_ino;
+        ++(*lastUsedInodeIndex);
+      }
     }
   }
 
@@ -140,7 +169,7 @@ int diskUsage(char *directoryPath) {
       // working directory.
       if ((dirstat.st_mode & S_IFMT) == S_IFDIR) {
         if (strcmp(".", directoryEntry->d_name) != 0) {
-          size = diskUsage(fullPath);
+          size = diskUsage(fullPath, seenInodes, lastUsedInodeIndex, seenInodesLength);
           fprintf(stdout, "%-8d%s\n", size, fullPath);
         }
 
@@ -156,4 +185,14 @@ int diskUsage(char *directoryPath) {
   free(fullPath);
   return total;
 }
+
+bool isInInodesArray(ino_t inode, ino_t *seenInodes, size_t length) {
+  for (size_t i = 0; i < length; ++i) {
+    if (*(seenInodes + i) == inode) {
+      return true;
+    }
+  }
+  return false;
+}
+
 
