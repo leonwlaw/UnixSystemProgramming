18af60dc1fcb7254b6a2e85af7c75a2cd33f20f7
leonwlaw <leonwlaw@gmail.com>
2013-11-11 23:25:46 -0500

    Initial commit

---
+++ chat/src/main.c
@@ -0,0 +1,22 @@
+/*
+Programmer: Leonard Law
+Purpose:
+  Class project for CS 239.
+
+  This is a simple 1:1 chat server.  Both server and client execute
+  this program.  The server will default to server mode if the mode is
+  not specified.
+
+  Usage:
+    chat [--server | --client] [interface:port]
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  return 0;
+}
+
