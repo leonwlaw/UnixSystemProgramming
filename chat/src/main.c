142ce93e2697da47cd3945ffc9f6a74fdfff01c9
leonwlaw <leonwlaw@gmail.com>
2013-11-12 00:03:45 -0500

    Added debug mode

--- chat/src/main.c
+++ chat/src/main.c
@@ -8,27 +8,54 @@ Purpose:
   not specified.
 
   Usage:
-    chat --server [interface] port
-    chat --client [interface] port
+    chat --server [interface] port [--debug]
+    chat --client [interface] port [--debug]
 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <stdbool.h>
 
 char *PROG_NAME;
+bool DEBUG = false;
 
 // The set of valid exit values.
 enum EXIT_T {
   EXIT_NORMAL = 0,
+  EXIT_ERROR_ARGUMENT,
   EXIT_ERROR_SOCKET,
 };
 
+/* --------------------------------------------------------------------
+Function declarations
+-------------------------------------------------------------------- */
+
+/*
+  Interprets command line arguments.
+  argc and argv are the # of command line arguments, and the array of
+  command line arguments respectively.
+
+  progName is the string pointer that should hold the executable's name.
+  debug corresponds to whether the user is requesting debug output.
+
+  If there is an unexpected argument in argv, this will cause the
+  program to *TERMINATE*.
+*/
+void parseArguments(
+  int argc, char **argv,
+  char **progName, bool *debug);
+
+
+/* --------------------------------------------------------------------
+Main
+-------------------------------------------------------------------- */
 int main(int argc, char **argv) {
-  PROG_NAME = argv[0];
+  parseArguments(argc, argv, &PROG_NAME, &DEBUG);
 
   // Used only if we're in server mode. This is where we'll listen for
   // incoming connections.
@@ -42,7 +69,30 @@ int main(int argc, char **argv) {
 
   fputs("Waiting for connection from a host...\n", stdout);
 
+  if (DEBUG) {
+    fputs("Connection complete.\n", stdout);
+  }
   // Got a connection, exit.
   close(serversocket);
   return 0;
+}
+
+/* --------------------------------------------------------------------
+Function definitions
+-------------------------------------------------------------------- */
+void parseArguments(
+  int argc, char **argv,
+  char **progName, bool *debug) {
+
+  *progName = *(argv++);
+
+  // Skip the first item, since that points to the executable.
+  for (--argc; argc > 0; --argc, ++argv) {
+    if (strcmp(*argv, "--debug") == 0) {
+      *debug = true;
+    } else {
+      fprintf(stderr, "%s: Unexpected argument '%s'", *progName, *argv);
+      exit(EXIT_ERROR_ARGUMENT);
+    }
+  }
 }
